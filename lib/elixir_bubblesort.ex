f7af0261967a727475f71428b45f08acc3fb48e1
Andrea Mazzarella <andrea.mazzarella89@gmail.com>
2016-12-01 11:50:04 +0000

    Refactor do_sort

--- lib/elixir_bubblesort.ex
+++ lib/elixir_bubblesort.ex
@@ -3,31 +3,27 @@ defmodule ElixirBubblesort do
   def sort([]), do: []
 
   def sort(list) do
-    do_sort(list)
+    do_sort([], list, :done)
   end
 
-  defp do_sort([a | [b | tail]]) when a > b do
-    do_sort([b], [a] ++ tail, :repeat_required)
+  
+  defp do_sort(partial_list, _rest_of_list = [a | [b | tail]], repeat_status) 
+  when a > b do
+    do_sort((partial_list++[b]), ([a]++tail), :repeat_required) 
   end
 
-  defp do_sort([a | [b | tail]]) when a <= b do
-    do_sort([a], [b] ++ tail, :done)
+  defp do_sort(partial_list, _rest_of_list = [a | [b | tail]], repeat_status) 
+  when a <= b do
+    do_sort((partial_list++[a]), ([b]++tail), repeat_status) 
   end
 
-  defp do_sort(partial_list, [a | [b | tail]], repeat_status) when a > b do
-    do_sort(partial_list ++ [b], [a] ++ tail, :repeat_required) 
+
+  defp do_sort(partially_sorted_list, last_node, :repeat_required) do
+    sort(partially_sorted_list++last_node)
   end
 
-  defp do_sort(partial_list, [a | [b | tail]], repeat_status) when a <= b do
-    do_sort(partial_list ++ [a], [b] ++ tail, repeat_status) 
-  end
-
-  defp do_sort(almost_full, last_node, :repeat_required) do
-    sort(almost_full ++ last_node)
-  end
-
-  defp do_sort(almost_full, last_node, :done) do
-    almost_full ++ last_node
+  defp do_sort(sorted_list, last_node, :done) do
+    sorted_list++last_node
   end
 
 end
--- test/elixir_bubblesort_test.exs
+++ test/elixir_bubblesort_test.exs
@@ -1,29 +1,38 @@
 defmodule ElixirBubblesortTest do
   use ExUnit.Case
 
-  test "handles an empty list" do
-    assert ElixirBubblesort.sort([]) === []
-  end
+  describe "bubble sorting" do
+    test "handles an empty list" do
+      assert ElixirBubblesort.sort([]) === []
+    end
 
-  test "leaves an already ordered list as it is" do
-    assert ElixirBubblesort.sort([1,2,5,111]) === [1,2,5,111]
-  end
+    test "leaves an already ordered list as it is" do
+      assert ElixirBubblesort.sort([1,2,5,111]) === [1,2,5,111]
+    end
 
-  test "sorts a two-nodes list" do
-    assert ElixirBubblesort.sort([2,1]) === [1,2]
-  end
+    test "sorts a two-nodes list" do
+      assert ElixirBubblesort.sort([2,1]) === [1,2]
+    end
 
-  test "sorts a longer list with just one iteration required" do
-    assert ElixirBubblesort.sort([1,2,4,3,6,5]) === [1,2,3,4,5,6]
-  end
+    test "sorts a longer list with just one iteration required" do
+      assert ElixirBubblesort.sort([1,2,4,3,6,5]) === [1,2,3,4,5,6]
+    end
 
-  test "sorts a longer list with two iterations required" do
-    assert ElixirBubblesort.sort([1,2,4,5,3,6]) === [1,2,3,4,5,6]
-  end
+    test "sorts a longer list with two iterations required" do
+      assert ElixirBubblesort.sort([1,2,4,5,3,6]) === [1,2,3,4,5,6]
+    end
 
-  test "equal numbers are supported" do
-    assert ElixirBubblesort.sort([1,2,2,5,3,6]) === [1,2,2,3,5,6]
-  end
+    test "equal numbers are supported" do
+      assert ElixirBubblesort.sort([1,2,2,5,3,6]) === [1,2,2,3,5,6]
+    end
 
+    test "larger example with many iterations required" do
+      assert ElixirBubblesort.sort([9,8,7,6,5,4,3,2,1]) === [1,2,3,4,5,6,7,8,9]
+    end
+
+    test "handles negative numbers correctly" do
+      assert ElixirBubblesort.sort([5,0,-22,-3]) === [-22,-3,0,5]
+    end
+  end
 
 end
